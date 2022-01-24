c6a0b9bfe265d2102d37735439d95ac310958a78
Henry Derbyshire <henry@htderbyshire.com>
2022-01-24 09:57:42 +0000

    retrieved information from api and displayed it on page

---
+++ src/index.js
@@ -0,0 +1,51 @@
+function getObject(data) {
+  const { main, description } = data.weather[0];
+  const { country } = data.sys;
+  const { name, main: weatherData } = data;
+
+  return {
+    name,
+    country,
+    main,
+    description,
+    weatherData,
+  };
+}
+
+async function getWeather(location) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=bc268a30fc568797b9cf81bb754ba0f4`,
+      { mode: "cors" }
+    );
+    const data = await response.json();
+    return getObject(data);
+  } catch (error) {
+    return error;
+  }
+}
+
+const input = document.querySelector("#location");
+const submit = document.querySelector(".submit");
+const weatherContainer = document.querySelector(".weather-info");
+
+function displayData(data) {
+  const html = `
+    <p>${data.name}, ${data.country}</p>
+      <p>${data.description}</p>
+      <ul>
+        <li>Temperature: ${Math.ceil(data.weatherData.temp - 273.15)}&#8451</li>
+        <li>Feels like: ${Math.ceil(
+          data.weatherData.feels_like - 273.15
+        )}&#8451</li>
+        <li>Humidity: ${data.weatherData.humidity}%</li>
+      </ul>`;
+  weatherContainer.innerHTML = html;
+}
+
+submit.addEventListener("click", (event) => {
+  event.preventDefault();
+  getWeather(input.value).then((value) => {
+    displayData(value);
+  });
+});
